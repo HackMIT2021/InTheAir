320a5e694915354db01dba83e05ece9b03f5e6fe
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 10:30:04 -0500

    Set up web app

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+//=================================================================================================
+
+const DatabaseURL = "mongodb://localhost:27017/InTheAir";
+
+const mongoose = require("mongoose");
+
+mongoose.connect(DatabaseURL, {
+	useNewURLParser: true,
+	useUnifiedTopology: true,
+	serverSelectionTimeoutMS: 10000,
+});
+
+const dbConnect = mongoose.connection;
+dbConnect.on("error", console.error.bind(console, "Database connection error:"));
+dbConnect.once("open", () => {
+	console.log("Connected to database");
+});
+
+//=================================================================================================
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("ejs", ejsMate);
+
+//=================================================================================================
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+	res.status(404).send("Error 404 Not Found");
+});
+
+//=================================================================================================
+
+const Port = 3000;
+
+app.listen(Port, () => {
+	console.log(`InTheAir is online on port ${Port}`);
+});
