bb207311a39a84480d7282f67402bf19b162847e
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 15:41:12 -0500

    add createReport controller

--- controllers/reports_Control.js
+++ controllers/reports_Control.js
@@ -9,6 +9,25 @@ const reportForm = (req, res) => {
 	res.render("Reports/new");
 };
 
+const addReport = async (req, res) => {
+	console.log(new Date());
+	// const geoData = await geocoderClient
+	// 	.forwardGeocode({
+	// 		query: req.body.newCamp.location,
+	// 		limit: 1,
+	// 	})
+	// 	.send();
+
+	const { name, description } = req.body.newReport;
+	const report = new Report(req.body.newReport);
+
+	report.author = req.user._id;
+	report.geometry = geoData.body.features[0].geometry;
+
+	await report.save();
+	res.redirect("/reports");
+};
+
 //=================================================================================================
 
-module.exports = { reportmap, reportForm };
+module.exports = { reportmap, reportForm, addReport };
