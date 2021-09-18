496230bea5b1e03a13ce6e64e335b848d5f9e26f
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 13:27:31 -0500

    Update reports on map

--- public/js/loadmap.js
+++ public/js/loadmap.js
@@ -1,49 +1,50 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGhydXYwODExIiwiYSI6ImNrdHAyNzF3dzA2Y20zMHB1cGpjcDBhNTIifQ.z09KTM7QCabwRTJ0ljiOng';
+console.log(markers);
+mapboxgl.accessToken =
+	"pk.eyJ1IjoiZGhydXYwODExIiwiYSI6ImNrdHAyNzF3dzA2Y20zMHB1cGpjcDBhNTIifQ.z09KTM7QCabwRTJ0ljiOng";
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-79.4512, 43.6568],
-    zoom: 4
+	container: "map",
+	style: "mapbox://styles/mapbox/streets-v11",
+	center: [-79.4512, 43.6568],
+	zoom: 4,
 });
-
 
 // Add the control to the map.
 map.addControl(
-    new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    })
+	new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl,
+	})
 );
 
 // Add geolocate control to the map.
 map.addControl(
-    new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        // When active the map will receive updates to the device's location as it changes.
-        trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        showUserHeading: true
-    })
+	new mapboxgl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true,
+		},
+		// When active the map will receive updates to the device's location as it changes.
+		trackUserLocation: true,
+		// Draw an arrow next to the location dot to indicate which direction the device is heading.
+		showUserHeading: true,
+	})
 );
 
-map.on('load', () => {
-map.addSource('earthquakes', {
-type: 'geojson',
-// Use a URL for the value for the `data` property.
-data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
-});
- 
-map.addLayer({
-'id': 'earthquakes-layer',
-'type': 'circle',
-'source': 'earthquakes',
-'paint': {
-    'circle-radius': 8,
-    'circle-stroke-width': 2,
-    'circle-color': 'red',
-    'circle-stroke-color': 'white'
-}
-});
+map.on("load", () => {
+	map.addSource("reports", {
+		type: "geojson",
+		// Use a URL for the value for the `data` property.
+		data: markers,
+	});
+
+	map.addLayer({
+		id: "reports-layer",
+		type: "circle",
+		source: "reports",
+		paint: {
+			"circle-radius": 8,
+			"circle-stroke-width": 2,
+			"circle-color": "red",
+			"circle-stroke-color": "white",
+		},
+	});
 });
