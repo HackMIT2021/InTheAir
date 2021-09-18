bcc9191753a77b02bb95fecca7e26519463cfdc2
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 11:15:56 -0500

    add User MongoDB model

---
+++ models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", UserSchema);
+
+//=================================================================================================
+
+module.exports = User;
