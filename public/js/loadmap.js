d1e3925db20d9a9a9bc3afad8fb1e999a0834ff3
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 13:56:14 -0500

    redo loadmap

--- public/js/loadmap.js
+++ public/js/loadmap.js
@@ -1,53 +1,115 @@
-console.log(markers);
 mapboxgl.accessToken =
 	"pk.eyJ1IjoiZGhydXYwODExIiwiYSI6ImNrdHAyNzF3dzA2Y20zMHB1cGpjcDBhNTIifQ.z09KTM7QCabwRTJ0ljiOng";
-const map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
 	container: "map",
 	style: "mapbox://styles/mapbox/streets-v11",
-	center: [-79.4512, 43.6568],
-	zoom: 4,
+	center: [-103.59179687498357, 40.66995747013945],
+	zoom: 3,
 });
 
-// Add the control to the map.
+map.addControl(new mapboxgl.NavigationControl());
 
-map.addControl(
-	new MapboxGeocoder({
-		accessToken: mapboxgl.accessToken,
-		mapboxgl: mapboxgl,
-	}),
-	"top-right"
-);
-
-// Add geolocate control to the map.
-
-map.addControl(
-	new mapboxgl.GeolocateControl({
-		positionOptions: {
-			enableHighAccuracy: true,
-		},
-		// When active the map will receive updates to the device's location as it changes.
-		trackUserLocation: true,
-		// Draw an arrow next to the location dot to indicate which direction the device is heading.
-		showUserHeading: true,
-	})
-);
-
-map.on("load", () => {
-	map.addSource("reports", {
+map.on("load", function () {
+	// Add a new source from our GeoJSON data and
+	// set the 'cluster' option to true. GL-JS will
+	// add the point_count property to your source data.
+	map.addSource("markers", {
 		type: "geojson",
-		// Use a URL for the value for the `data` property.
+		// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+		// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
 		data: markers,
+		cluster: true,
+		clusterMaxZoom: 14, // Max zoom to cluster points on
+		clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
 	});
 
 	map.addLayer({
-		id: "reports-layer",
+		id: "clusters",
 		type: "circle",
-		source: "reports",
+		source: "markers",
+		filter: ["has", "point_count"],
 		paint: {
-			"circle-radius": 8,
-			"circle-stroke-width": 2,
-			"circle-color": "red",
-			"circle-stroke-color": "white",
+			// Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+			// with three steps to implement three types of circles:
+			//   * Blue, 20px circles when point count is less than 100
+			//   * Yellow, 30px circles when point count is between 100 and 750
+			//   * Pink, 40px circles when point count is greater than or equal to 750
+			"circle-color": [
+				"step",
+				["get", "point_count"],
+				"#00BCD4",
+				10,
+				"#3296F3",
+				30,
+				"#3F51B5",
+			],
+			"circle-radius": ["step", ["get", "point_count"], 15, 10, 22, 30, 29],
 		},
+	});
+
+	map.addLayer({
+		id: "cluster-count",
+		type: "symbol",
+		source: "markers",
+		filter: ["has", "point_count"],
+		layout: {
+			"text-field": "{point_count_abbreviated}",
+			"text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+			"text-size": 12,
+		},
+	});
+
+	map.addLayer({
+		id: "unclustered-point",
+		type: "circle",
+		source: "markers",
+		filter: ["!", ["has", "point_count"]],
+		paint: {
+			"circle-color": "#11b4da",
+			"circle-radius": 4,
+			"circle-stroke-width": 1,
+			"circle-stroke-color": "#fff",
+		},
+	});
+
+	// // inspect a cluster on click
+	// map.on("click", "clusters", function (e) {
+	// 	var features = map.queryRenderedFeatures(e.point, {
+	// 		layers: ["clusters"],
+	// 	});
+	// 	var clusterId = features[0].properties.cluster_id;
+	// 	map.getSource("campgrounds").getClusterExpansionZoom(clusterId, function (err, zoom) {
+	// 		if (err) return;
+
+	// 		map.easeTo({
+	// 			center: features[0].geometry.coordinates,
+	// 			zoom: zoom,
+	// 		});
+	// 	});
+	// });
+
+	// // When a click event occurs on a feature in
+	// // the unclustered-point layer, open a popup at
+	// // the location of the feature, with
+	// // description HTML from its properties.
+	// map.on("click", "unclustered-point", function (e) {
+	// 	var coordinates = e.features[0].geometry.coordinates.slice();
+
+	// 	const { popUpMarkup } = e.features[0].properties;
+	// 	// Ensure that if the map is zoomed out such that
+	// 	// multiple copies of the feature are visible, the
+	// 	// popup appears over the copy being pointed to.
+	// 	while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+	// 		coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+	// 	}
+
+	// 	new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
+	// });
+
+	map.on("mouseenter", "clusters", function () {
+		map.getCanvas().style.cursor = "pointer";
+	});
+	map.on("mouseleave", "clusters", function () {
+		map.getCanvas().style.cursor = "";
 	});
 });
