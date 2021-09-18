2c31b58a1ea7d454efeb2e2bd0faad1b25b7670d
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 10:57:04 -0500

    Add error handling

---
+++ utils/ExpressError.js
@@ -0,0 +1,11 @@
+class ExpressError extends Error {
+	constructor(statusCode, message) {
+		super();
+		this.statusCode = statusCode;
+		this.message = message;
+	}
+}
+
+//=================================================================================================
+
+module.export = ExpressError;
