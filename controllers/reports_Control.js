729381312a0359118d9cbefc4807005d10c6772b
Max <80323354+GuyBritish@users.noreply.github.com>
2021-09-18 21:34:45 -0500

    Add edit report functionality

--- controllers/reports_Control.js
+++ controllers/reports_Control.js
@@ -1,4 +1,7 @@
 const Report = require("../models/Reports");
+const User = require("../models/Users");
+
+//=================================================================================================
 
 const reportmap = async (req, res) => {
 	let places = await Report.find({});
@@ -21,7 +24,6 @@ const showReport = async (req, res) => {
 
 const addReport = async (req, res) => {
 	let currDate = new Date();
-	console.log(currDate);
 	// const geoData = await geocoderClient
 	// 	.forwardGeocode({
 	// 		query: req.body.newCamp.location,
@@ -36,6 +38,11 @@ const addReport = async (req, res) => {
 	report.author = req.user._id;
 	report.geometry = { type: "Point", coordinates: [req.body.lng, req.body.lat] };
 
+	const targetUser = await User.findById(req.user._id);
+	targetUser.report.hasReport = true;
+	targetUser.report.curr = report._id;
+
+	await targetUser.save();
 	await report.save();
 	req.flash("success", "Successfully created a new report!");
 	res.redirect("/reports");
@@ -51,7 +58,20 @@ const editForm = async (req, res) => {
 	res.render("Reports/edit", { report });
 };
 
-const editReport = async (req, res) => {};
+const editReport = async (req, res) => {
+	const { id } = req.params;
+	let currDate = new Date();
+	const { name, description } = req.body.newReport;
+	const target = await Report.findById(id);
+	target.date = currDate;
+	target.name = name;
+	target.description = description;
+	target.geometry = { type: "Point", coordinates: [req.body.lng, req.body.lat] };
+
+	await target.save();
+	req.flash("success", "Successfully updated report!");
+	res.redirect(`/reports/${target._id}`);
+};
 
 const destroyReport = async (req, res) => {};
 
